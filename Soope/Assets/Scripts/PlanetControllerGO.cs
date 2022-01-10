fb0f045a6489bd9b3f85d112aa8d6d11cf3f7865
Donny The newspaper <Donnykurniawan1119@gmail.com>
2022-01-10 20:01:47 +0700

    GO planets

---
+++ Soope/Assets/Scripts/PlanetControllerGO.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetControllerGO : MonoBehaviour
+{
+    public GameObject[] Planets;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
