01b17290133f5548e8ad267d30c5d6a6aef35a85
Donny The newspaper <Donnykurniawan1119@gmail.com>
2022-01-10 20:20:04 +0700

    DONE

--- Soope/Assets/Scripts/PlanetControllerGO.cs
+++ Soope/Assets/Scripts/PlanetControllerGO.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class PlanetControllerGO : MonoBehaviour
 {
     public GameObject[] Planets;
+
+    Queue<GameObject> availablePlanets = new Queue<GameObject>();
     void Start()
     {
-        
+        availablePlanets.Enqueue(Planets [0]);
+        availablePlanets.Enqueue(Planets [1]);
+        availablePlanets.Enqueue(Planets [2]);
+
+        InvokeRepeating("MovePlaneDown", 0, 20f);
     }
 
 
@@ -15,4 +21,24 @@ public class PlanetControllerGO : MonoBehaviour
     {
         
     }
+    void MovePlaneDown()
+    {
+        if (availablePlanets.Count == 0)
+            return;
+        GameObject aPlanet = availablePlanets.Dequeue();
+
+    }
+    void EnqueuePlanets()
+    {
+        foreach(GameObject aPlanet in Planets)
+        {
+            if ((aPlanet.transform.position.y < 0) && (!aPlanet.GetComponent<Planet>().isMoving))
+            {
+                aPlanet.GetComponent<Planet>().ResetPosition();
+
+                availablePlanets.Enqueue(aPlanet);
+            }
+        }
+    }
+
 }
