96a26911a1a0af6254b86b4b89bddbb32f2deea0
Donny The newspaper <Donnykurniawan1119@gmail.com>
2022-09-23 10:09:59 +0700

    swe

--- Flatter burd/Assets/bird.cs
+++ Flatter burd/Assets/bird.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class birdup : MonoBehaviour
+public class bird : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(message: "Start");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(message: "Update");
     }
 }
